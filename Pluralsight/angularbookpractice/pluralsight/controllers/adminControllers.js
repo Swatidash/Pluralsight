342812dfc79dbd0d20884a4506102ddf0949b095
Swatidash <swatidash2007@gmail.com>
2016-09-27 09:19:31 -0700

    hi

---
+++ Pluralsight/angularbookpractice/pluralsight/controllers/adminControllers.js
@@ -0,0 +1,58 @@
+﻿/// <reference path="../app.html" />
+angular.module("sportsStoreAdmin")
+.constant("authUrl", function ($scope, $http, $location, authUrl) {
+
+    $scope.authentication = function (user, pass) {
+        $http.post(authUrl, {
+            username: user,
+            password: pass
+        }, {
+            withCredentials: true
+        }).success(function (data) {
+            $location.path("/main");
+        }).error(function (error) {
+            $scope.authentication = error;
+        });
+    }
+})
+.controller("mainCtrl", function ($scope) {
+
+    $scope.screens = ["Products", "Orders"];
+    $scope.current = $scope.screens[0];
+
+    $scope.setScreen = function (index) {
+        $scope.current = $scope.screens[index];
+    };
+
+    $scope.getScreen = function () {
+        return $scope.current == "Products"
+          ? "/views/adminProducts.html" : "/views/adminOrders.html";
+    };
+})
+.controller("orderCtrl", function ($scope, $http, orderUrl) {
+
+    $http.get(orderUrl, { withCredentials: true })
+    .success(function (data) {
+        $scope.orders = data;
+    })
+    .error(function (error) {
+        $scope.error = error;
+    });
+
+    $scope.selectedOrder;
+
+    $scope.selectedOrder = order;
+});
+
+$scope.calcTotal = function (order) {
+    var total = 0;
+    for (var i = 0; i < order.products.length; i++) {
+        total +=
+            order.products[i].count * order.products[i].price;
+    }
+    return total;
+
+    }
+
+});
+ 
---
+++ Pluralsight/angularbookpractice/pluralsight/controllers/top-level controller.js
@@ -0,0 +1,24 @@
+﻿/// <reference path="../app.html" />
+
+
+app.controller('sportsStoreCtrl', [$scope, $http, $location,
+    function ($scope, $http, $location) {
+        $scope.data = [];
+ 
+        function getData() {
+            $http({
+                method: "GET",
+                url: "http://localhost:5500/products",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': '*'
+                }
+            }).then(function (response) {
+                //set response json studentlist
+                $scope.data = response.data;
+            });
+        };
+
+        getData();
+    
+]);
